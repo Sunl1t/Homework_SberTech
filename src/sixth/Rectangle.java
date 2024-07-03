342c6ea27e04e711277d2692ffb2340597062162
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-03 19:29:44 +0300

    Update Rectangle.java

--- src/sixth/Rectangle.java
+++ src/sixth/Rectangle.java
@@ -10,18 +10,22 @@ public class Rectangle extends Figure implements Drawable {
         this.height = height;
     }
 
+    @Override
     public double area() {
         return width * height;
     }
 
+    @Override
     public double perimeter() {
         return 2 * (width + height);
     }
 
+    @Override
     public void draw() {
         System.out.println("Прямоугольник нарисован по координатам " + point + " шириной " + width + " и длиной " + height + ", чёрного цвета.");
     }
 
+    @Override
     public void draw(Color color) {
         System.out.println("Прямоугольник нарисован по координатам " + point + " шириной " + width + " и длиной ," + height + " "+ color + " цвета.");
     }
