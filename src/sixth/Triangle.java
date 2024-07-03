af3b697d6820c5b2fa57bd7d152765c869e4f610
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-03 19:27:37 +0300

    Update Triangle.java

--- src/sixth/Triangle.java
+++ src/sixth/Triangle.java
@@ -10,19 +10,23 @@ public class Triangle extends Figure implements Drawable {
         this.c = c;
     }
 
+    @Override
     public double area() {
         double s = (a + b + c) / 2;
         return Math.sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
+    @Override
     public double perimeter() {
         return a + b + c;
     }
 
+    @Override
     public void draw() {
         System.out.println("Треугольник нарисован по координатам " + point + " со сторонами " + a + ", " + b + ", " + c + " чёрного цвета.");
     }
 
+    @Override
     public void draw(Color color) {
         System.out.println("Треугольник нарисован по координатам " + point + " со сторонами " + a + ", " + b + ", " + c + " "+ color + " цвета.");
     }
