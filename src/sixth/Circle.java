11a04c81d28ee6c2fc756c7838d2aac8d82e23a5
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-03 19:32:03 +0300

    Update Circle.java

--- src/sixth/Circle.java
+++ src/sixth/Circle.java
@@ -10,21 +10,25 @@ public class Circle extends Figure implements Drawable
         this.radius = radius;
     }
 
+    @Override
     public double area()
     {
         return Math.PI * radius * radius;
     }
 
+    @Override
     public double perimeter()
     {
         return 2 * Math.PI * radius;
     }
 
+    @Override
     public void draw()
     {
         System.out.println("Круг нарисован по координатам " + point + " радиуса " + radius + " чёрного цвета.");
     }
 
+    @Override
     public void draw(Color color) {
         System.out.println("Круг нарисован по координатам " + point + " радиуса " + radius +" "+ color + " цвета.");
     }
