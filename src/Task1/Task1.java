d125350119f8a5711857cba3d918a0e5de93dd41
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-04 22:15:49 +0300

    Update Task1.java

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -1,6 +1,6 @@
-package Task1;
+package task1;
 
-public class Task1 {
+public class task1 {
     public static void main(String[] args) {
         for (int i = 1; i < 15; i++) {
             System.out.print(i*7 + " ");
