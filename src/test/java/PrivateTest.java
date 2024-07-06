96ef072bb5d189da7c96b9f8a41c4e1d5016423e
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-06 13:35:57 +0300

    Update PrivateTest.java

--- src/test/java/PrivateTest.java
+++ src/test/java/PrivateTest.java
@@ -1,14 +1,14 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import Sunlit.unit_tests.FirstClass;
+import Sunlit.unit_tests.SecondClass;
 import java.lang.reflect.Method;
-
 import org.junit.jupiter.api.Test;
-
 public class PrivateTest
 {
     @Test
     public void testPrivateMethod() throws Exception {
-        FirstClass instance = new FirstClass();
+        SecondClass secondClassMock = Mockito.mock(SecondClass.class);
+        FirstClass instance = new FirstClass(secondClassMock);
         Method method = FirstClass.class.getDeclaredMethod("privateMethod");
         method.setAccessible(true);
         String result = (String) method.invoke(instance);
