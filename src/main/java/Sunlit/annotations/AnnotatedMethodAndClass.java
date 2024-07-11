6e260e19ba3f6dc9573bd44badb8ef6949266e44
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-11 14:30:58 +0300

    Update AnnotatedMethodAndClass.java

--- src/main/java/Sunlit/annotations/AnnotatedMethodAndClass.java
+++ src/main/java/Sunlit/annotations/AnnotatedMethodAndClass.java
@@ -1,18 +1,19 @@
-package Sunlit.annotations;
+package sunlit.annotations;
 
 import lombok.NonNull;
 
 @InheritanceAnnotation(dayOfWeek = 0)
 public class AnnotatedMethodAndClass extends AnnotatedClass {
     @TimeAndPriority(hour = 11, priority = 1, description = "The most important task")
-
+    @TimeAndPriority(hour = 13, priority = 2, description = "Important task")
+    @TimeAndPriority(hour = 16, priority = 3, description = "Just a task")
     public void dataContainerProcess(@NonNull DataContainer dataContainer) {
         new DataContainer(dataContainer.getKey(), dataContainer.getValue());
     }
 
-@InheritanceAnnotation(dayOfWeek = 5)
+
 public static class AnnotatedMethodAndSubclass extends AnnotatedClass{
-    @TimeAndPriority(hour = 2, priority = 9, description = "Unimportant task")
+    @TimeAndPriority(hour = 23, priority = 9, description = "Unimportant task")
 
     public static boolean isEmptyDataContainer(@NonNull DataContainer dataContainer) {
         return dataContainer.getValue() == 0 && dataContainer.getKey().isEmpty();
