57fcf8fb1192545a3f91da254e2dc734960f1169
Sergei <145154426+Sunl1t@users.noreply.github.com>
2024-07-11 19:01:04 +0300

    Update Reflection.java

--- src/main/java/Sunlit/reflection/Reflection.java
+++ src/main/java/Sunlit/reflection/Reflection.java
@@ -1,84 +1,93 @@
 package Sunlit.reflection;
-import Sunlit.annotations.AnnotatedMethodAndClass;
-import Sunlit.annotations.DataContainer;
-import Sunlit.annotations.TimeAndPriorities;
-import Sunlit.annotations.TimeAndPriority;
+import Sunlit.annotations.*;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
 
 
 public class Reflection {
-    public static void annotatedClassCreated (int dayOfWeek, int value, String key) {
-        DataContainer dataContainer;
-        try {
-            dataContainer = new DataContainer(key, value);
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
+    public static void annotatedClassCreated(int dayOfWeek, int value, String key) {
+        DataContainer dataContainer = new DataContainer(key, value);
+
+        List<Object> instances = createInstancesForDayOfWeek(dayOfWeek);
+
+        invokeMethodsByPriority(instances, dataContainer);
+    }
+
+    private static List<Object> createInstancesForDayOfWeek(int dayOfWeek) {
+        List<Object> instances = new ArrayList<>();
+
+        Class<?>[] classes = {AnnotatedClass.class,AnnotatedMethodAndClass.class,AnnotatedMethodAndClass.AnnotatedMethodAndSubclass.class};
+
+        for (Class<?> clazz : classes) {
+            InheritanceAnnotation annotation = clazz.getAnnotation(InheritanceAnnotation.class);
+            if (annotation != null && annotation.dayOfWeek() == dayOfWeek) {
+                try {
+                    Object instance = clazz.getDeclaredConstructor().newInstance();
+                    instances.add(instance);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return instances;
+    }
+
+    private static void invokeMethodsByPriority(List<Object> instances, DataContainer dataContainer) {
+        List<Method> methodList = new ArrayList<>();
+
+        for (Object instance : instances) {
+            Method[] allMethods = instance.getClass().getDeclaredMethods();
+            for (Method method : allMethods) {
+                if (method.isAnnotationPresent(TimeAndPriority.class) || method.isAnnotationPresent(TimeAndPriorities.class)) {
+                    methodList.add(method);
+                }
+            }
         }
 
-        try {
-            dataContainer = DataContainer.class.getConstructor(int.class, String.class).newInstance(value, key);
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
-        }
-
-        Queue<Method> methodQueue = new PriorityQueue<>((x, y) -> {
-            int priorityX = 0;
-            if (x.isAnnotationPresent(TimeAndPriority.class)) {
-                priorityX = x.getAnnotation(TimeAndPriority.class).priority();
-            } else if (x.isAnnotationPresent(TimeAndPriorities.class)) {
-                TimeAndPriority[] timePriorities = x.getAnnotation(TimeAndPriorities.class).value();
-                int min = timePriorities[0].priority();
-                for (int timePriorityNumber = 1; timePriorityNumber < timePriorities.length; timePriorityNumber++) {
-                    min = Math.min(min, timePriorities[timePriorityNumber].priority());
-                }
-                priorityX = min;
-            }
-            int priorityY = 0;
-            if (y.isAnnotationPresent(TimeAndPriority.class)) {
-                priorityY = y.getAnnotation(TimeAndPriority.class).priority();
-            } else if (y.isAnnotationPresent(TimeAndPriorities.class)) {
-                TimeAndPriority[] timePriorities = y.getAnnotation(TimeAndPriorities.class).value();
-                int min = timePriorities[0].priority();
-                for (int timePriorityNumber = 1; timePriorityNumber < timePriorities.length; timePriorityNumber++) {
-                    min = Math.min(min, timePriorities[timePriorityNumber].priority());
-                }
-                priorityY = min;
-            }
+        methodList.sort((x, y) -> {
+            int priorityX = getMethodPriority(x);
+            int priorityY = getMethodPriority(y);
             return Integer.compare(priorityX, priorityY);
         });
 
-        try {
-            Method[] classMethod = AnnotatedMethodAndClass.class.getMethods();
-            for (Method method: classMethod) {
-                if (method.isAnnotationPresent(TimeAndPriority.class) || method.isAnnotationPresent(TimeAndPriorities.class)) {
-                    methodQueue.add(method);
+        for (Method method : methodList) {
+            try {
+                if (method.getParameterCount() == 1) {
+                    method.invoke(method.getDeclaringClass().newInstance(), dataContainer);
                 }
+                checkDataContainerState(dataContainer);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            for (Method method: methodQueue) {
-                if (method.getParameters().length == 2) {
-                    method.invoke(dataContainer, dataContainer, dataContainer);
-                } else if (method.getParameters().length == 1) {
-                    method.invoke(dataContainer, dataContainer);
-                }
-            }
-
-        } catch (Exception e) {
-            throw new Error(e.getMessage());
         }
+    }
 
+    private static int getMethodPriority(Method method) {
+        int priority = Integer.MAX_VALUE;
+
+        if (method.isAnnotationPresent(TimeAndPriority.class)) {
+            priority = method.getAnnotation(TimeAndPriority.class).priority();
+        } else if (method.isAnnotationPresent(TimeAndPriorities.class)) {
+            TimeAndPriority[] timePriorities = method.getAnnotation(TimeAndPriorities.class).value();
+            for (TimeAndPriority tp : timePriorities) {
+                if (tp.priority() < priority) {
+                    priority = tp.priority();
+                }
+            }
+        }
+        return priority;
+    }
+    
+    private static void checkDataContainerState(DataContainer dataContainer) {
         try {
             Field keyField = dataContainer.getClass().getDeclaredField("key");
             Field valueField = dataContainer.getClass().getDeclaredField("value");
-
             keyField.setAccessible(true);
             valueField.setAccessible(true);
-
             String classKey = (String) keyField.get(dataContainer);
             int classValue = valueField.getInt(dataContainer);
-
             System.out.println("DataContainer state: key = " + classKey + ", value = " + classValue);
         } catch (Exception e) {
             throw new Error(e.getMessage());
